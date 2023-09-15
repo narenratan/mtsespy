33fe86ac2268cace48cbf8459054ec5524121ce8
Naren Ratan <narenr@fastmail.com>
2023-09-15 20:38:14 +0100

    Use Master context manager in multichannel script

--- multi_channel.py
+++ multi_channel.py
@@ -8,15 +8,17 @@ Calling as
 puts the tuning from example.scl on channel 0, example_2.scl on channel 1, and so on.
 """
 import sys
+import signal
 
 import mtsespy as mts
 
-mts.register_master()
+with mts.Master():
+    for i in range(16):
+        mts.set_multi_channel(True, i)
 
-for i in range(16):
-    mts.set_multi_channel(True, i)
+    for channel, (scl_file, kbm_file) in enumerate(zip(sys.argv[1::2], sys.argv[2::2])):
+        freqs = mts.scala_files_to_frequencies(scl_file, kbm_file)
+        for note, f in enumerate(freqs):
+            mts.set_multi_channel_note_tuning(f, note, channel)
 
-for channel, (scl_file, kbm_file) in enumerate(zip(sys.argv[1::2], sys.argv[2::2])):
-    freqs = mts.scala_files_to_frequencies(scl_file, kbm_file)
-    for note, f in enumerate(freqs):
-        mts.set_multi_channel_note_tuning(f, note, channel)
+    signal.pause()
