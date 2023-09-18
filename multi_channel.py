668157ea105635eb3749dac83132faaedfe41840
Naren Ratan <narenr@fastmail.com>
2023-09-18 21:17:31 +0100

    Use set_multi_channel_note_tunings in multi channel script

--- multi_channel.py
+++ multi_channel.py
@@ -3,9 +3,9 @@ Set multi-channel tuning from scala files
 
 Calling as
 
-    $ python3 -i multi_channel_from_scala.py example.scl example.kbm example_2.scl example_2.kbm
+    $ python3 -i multi_channel_from_scala.py example_1.scl example_1.kbm example_2.scl example_2.kbm
 
-puts the tuning from example.scl on channel 0, example_2.scl on channel 1, and so on.
+puts the tuning from example_1.scl on channel 0, example_2.scl on channel 1, and so on.
 """
 import sys
 import signal
@@ -13,12 +13,12 @@ import signal
 import mtsespy as mts
 
 with mts.Master():
-    for i in range(16):
-        mts.set_multi_channel(True, i)
-
     for channel, (scl_file, kbm_file) in enumerate(zip(sys.argv[1::2], sys.argv[2::2])):
+        print(
+            f"Setting tuning for midi channel {channel} from {scl_file} and {kbm_file}"
+        )
+        mts.set_multi_channel(True, channel)
         freqs = mts.scala_files_to_frequencies(scl_file, kbm_file)
-        for note, f in enumerate(freqs):
-            mts.set_multi_channel_note_tuning(f, note, channel)
+        mts.set_multi_channel_note_tunings(freqs, channel)
 
     signal.pause()
